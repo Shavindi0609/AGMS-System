ed28ae5221e0dc9f8602a5bf78f7c0d4522825ef
#Shanu0609@ <shavindialoka69@gmail.com>
2026-04-09 09:53:36 +0530

    Step 2: Setup API Gateway with routing configuration

--- api-gateway/api-gateway/src/main/java/lk/ijse/api_gateway/ApiGatewayApplication.java
+++ api-gateway/api-gateway/src/main/java/lk/ijse/api_gateway/ApiGatewayApplication.java
@@ -2,11 +2,14 @@ package lk.ijse.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
